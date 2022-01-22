71d71a8d83541a1143b01497c83bc26493c4b52d
Arne Welzel <arne.welzel@corelight.com>
2022-01-22 17:48:32 +0100

    doc: Remove orig_p/resp_p workaround including proto for examples/log-bypass.js

--- doc/examples/log-bypass.js
+++ doc/examples/log-bypass.js
@@ -14,18 +14,8 @@ zeek.hook('Log::log_stream_policy', -1000, function(rec, log_id) {
   // CamelCase to snake_case: PacketFilter to packet_filter
   log_id = log_id.replace(/([a-z0-9])([A-Z])/g, '\$1_\$2').toLowerCase()
 
-  // Copy the log record to make it mutable.
-  let copy = Object.assign({}, rec);
-
-  // Replace {"orig_p": {"port": <port> "proto": ...}} with {"orig_p": <port>}
-  if (copy.id?.orig_p?.port !== undefined) {
-    copy.id = Object.assign({}, rec.id);  // Copy the id record to mutate it.
-    copy.id['orig_p'] = rec.id.orig_p.port;
-    copy.id['resp_p'] = rec.id.resp_p.port;
-  }
-
-  // Write to a the log file. Synchronous here for simplicity.
-  fs.appendFileSync(log_id + '.log', JSON.stringify(copy) + '\n')
+  // Write to the log file. Synchronous here for simplicity.
+  fs.appendFileSync(log_id + '.log', JSON.stringify(rec) + '\n')
 
   // If you wanted to hand-off logs to a central Redis server.
   // redis_client.publish(log_id, JSON.stringify(copy));

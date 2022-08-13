e4448ac6d9da3f46fd0f8ac88bac1a5aa60bffad
Arne Welzel <arne.welzel@corelight.com>
2022-08-13 18:20:01 +0200

    test/get-stats: Make event_stats test fuzzy
    Seems Zeek 5.1 queues/dispatches a few more events. That
    was bound to break eventually :-)

--- tests/zeekjs/invoke/get-stats.sh
+++ tests/zeekjs/invoke/get-stats.sh
@@ -19,6 +19,10 @@ zeek.on('zeek_done', function() {
   let net_stats = zeek.invoke('get_net_stats');
   let event_stats = zeek.invoke('get_event_stats');
   console.log(JSON.stringify(net_stats, null, 2));
-  console.log(JSON.stringify(event_stats, null, 2));
+
+  console.log(`queued in event_stats ${'queued' in event_stats}`);
+  console.log(`dispatched in event_stats ${'dispatched' in event_stats}`);
+  console.log(`queued >= 40 ${event_stats['queued'] >= 40}`)
+  console.log(`dispatched >= 40 ${event_stats['dispatched'] >= 40}`)
 });
 @TEST-END-FILE

5de78b43cd69ff381ab74a9a856107a9187b1757
Seth Hall <seth@remor.com>
2025-03-31 09:30:42 -0400

    Ignore the typescript test.

--- tests/zeekjs/hello-typescript.sh
+++ tests/zeekjs/hello-typescript.sh
@@ -1,3 +1,6 @@
+# Ignore this test until the platforms where CI is running tests
+# have versions of Node.js >=22
+# @TEST-IGNORE
 # @TEST-EXEC: NODE_OPTIONS="--experimental-transform-types" zeek ./hello.ts
 # @TEST-EXEC: btest-diff .stdout
 # @TEST-GROUP: smoke
